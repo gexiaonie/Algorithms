0f4ea9912a36dcd2809d36be2063dff3e7685396
Cyrilplus <yong_cyril@163.com>
2016-01-03 20:47:33 +0800

    git commit -m "添加作者信息"

--- Sorted/Selecte/Select.py
+++ Sorted/Selecte/Select.py
@@ -1,10 +1,15 @@
 # -*-coding:utf-8 -*-
 '''
+作者：Cyril
+时间：2016年1月3号
+
 选择排序：
 	首先，找到数组中的最小元素，然后和第一个元素交换位置
 然后剩下的数组中再找到最小值和第二个数据交换，以此类推。
 提供了自定义比较函数
 '''
+
+
 def selectSorted(array, compare=cmp):
     count = len(array)
     for i in range(0, count):
@@ -20,9 +25,9 @@ def selectSorted(array, compare=cmp):
 
 if __name__ == '__main__':
     array = [8, 10, 5, 6, 1, 9]
-    #从大到小排序
-    selectSorted(array,lambda x,y:cmp(y,x))
+    # 从大到小排序
+    selectSorted(array, lambda x, y: cmp(y, x))
     print array
-    #默认从小到大排序
+    # 默认从小到大排序
     selectSorted(array)
     print array
