0e251177d9de6d45529fe24c8c2a2f8b5042ab20
Cyrilplus <yong_cyril@163.com>
2016-01-04 14:35:36 +0800

    添加注释以及作者信息

--- Sorted/Insert/insertWithChart.py
+++ Sorted/Insert/insertWithChart.py
@@ -7,9 +7,19 @@ import matplotlib.pyplot as plt
 
 def insertSorted(array, compare=cmp):
     count = len(array)
+    x = np.arange(count)
+    fig, axes = plt.subplots(ncols=1, nrows=count)
+    axesTulpe = axes.ravel()
+    axesTulpe[0].bar(x, np.array(array), 0.5)
     for i in range(1, count):
         # 将 array[j]插入到左边的有序数组中，丛右向左遍历
         for j in range(i, 0, -1):
             if compare(array[j], array[j - 1]) < 0:
                 # swap
                 array[j], array[j - 1] = array[j - 1], array[j]
+        axesTulpe[i].bar(x, np.array(array), 0.5)
+    plt.show()
+
+if __name__ == '__main__':
+    array = np.random.randint(1, 200, (1, 6))[0]
+    insertSorted(list(array))
--- Sorted/Shell/Shell.py
+++ Sorted/Shell/Shell.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 '''
+作者：Cyril
+时间：2016年1月4号
+
 希尔排序：
 	插入排序的升级版，相隔为h的序列是有序的，然后不断缩小h。
 知道h为1，这个最后一步就是插入操作。由于插入排序的性质，受输入
@@ -13,14 +17,18 @@ def shellSorted(array, compare=cmp):
     count = len(array)
     h = 1
     print count
+    # 希尔间隔算子，貌似中不同值效果不一样，也有关于最优的选择，
+    # 这里就不再讨论，有兴趣自行搜索
     while h < count / 3:
         h = h * 3 + 1
+    # 这里是三层循环，其实内两层循环和插入排序基本上一模一样
+    # 最外一层循环主要是取不同的间隔h，但最终间隔h＝1，最终使用插入排序
     while h >= 1:
         for i in range(h, count):
             for j in range(i, h - 1, -h):
                 if compare(array[j], array[j - h]) == -1:
                     array[j], array[j - h] = array[j - h], array[j]
-        h = h / 3
+        h = h / 3  # 间隔不断缩短直到1
 
 if __name__ == '__main__':
     array = [10, 4, 2, 5, 6, 1]
